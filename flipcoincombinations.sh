2489437a78fb72508471e6153eab4ee46b80598f
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-27 22:20:56 +0530

    Added code for flipping coin and storing triplet combination in dictionary and finding its_percentage

--- flipcoincombinations.sh
+++ flipcoincombinations.sh
@@ -4,7 +4,7 @@ echo
 #no of singlet combinatins = 2^1=2
 #they are heads and tails
 declare -A occurence_of_singlet=(  [H]=0 [T]=0 )
-echo "SINGLET COMBINATION"
+echo "SINGLET COMBINATIONS"
 for(( flip = 1; flip <= n; flip++ ))
 do
     echo -n "Flip-$flip is "
@@ -28,8 +28,8 @@ do
 done
 
 echo 
-echo "DOUBLET COMBINATION"
-declare -A occurence_of_doublet=( [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
+echo "DOUBLET COMBINATIONS"
+declare -A occurence_of_doublet=(     [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
 for(( flip = 1; flip <= n; flip++ ))
 do
     echo -n "Flip-$flip is "
@@ -57,6 +57,58 @@ done
 for combination in ${!occurence_of_doublet[@]}
 do
     count=${occurence_of_doublet[$combination]}
+    percentage_of_combination=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
+    echo "percentage of $combination combination is $percentage_of_combination"   
+done
+
+
+echo 
+echo "TRIPLET COMBINATIONS"
+
+declare -A occurence_of_triplet=(  [HHH]=0 [HHT]=0 [HTH]=0 [THH]=0 [HTT]=0 [THT]=0 [TTH]=0 [TTT]=0 )
+for(( flip = 1; flip <= n; flip++ ))
+do
+    echo -n "Flip-$flip is "
+    (( coin_flip_value = RANDOM % 8 ))
+    case $coin_flip_value in
+        0)
+            echo "Heads Heads Heads"
+            (( occurence_of_triplet[HHH]++ )) 
+            ;;
+        1)
+            echo "Heads Heads Tails"
+            (( occurence_of_triplet[HHT]++ ))
+            ;;
+        2)
+            echo "Heads Tails Heads"
+            (( occurence_of_triplet[HTH]++ ))
+            ;;
+        3)
+            echo "Tails Heads Heads"
+            (( occurence_of_triplet[THH]++ ))
+            ;;
+        4)
+            echo "Heads Tails Tails"
+            (( occurence_of_triplet[HTT]++ )) 
+            ;;
+        5)
+            echo "Tails Heads Tails"
+            (( occurence_of_triplet[THT]++ ))
+            ;;
+        6)
+            echo "Tails Tails Heads"
+            (( occurence_of_triplet[TTH]++ ))
+            ;;
+        7)
+            echo "Tails Tails Tails"
+            (( occurence_of_triplet[TTT]++ ))
+            ;;
+    esac
+done
+
+for combination in ${!occurence_of_triplet[@]}
+do
+    count=${occurence_of_triplet[$combination]}
     percentage_of_combination=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
     echo "percentage of $combination combination is $percentage_of_combination"   
 done
