72bba75ef63d5c0f7aa52de44f6eddf4d31f8614
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-27 22:18:33 +0530

    Added code for flipping coin and storing doublet combination in dictionary and finding its_percentage

--- flipcoincombinations.sh
+++ flipcoincombinations.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-read -p "Enter no of coin flips:" n
-#no of singlet combinations = 2^1=2
+read -p "Enter no of times to flip single coin:" n
+echo
+#no of singlet combinatins = 2^1=2
 #they are heads and tails
-declare -A occurence_of_singlet=( [heads]=0 [tails]=0 )
+declare -A occurence_of_singlet=(  [H]=0 [T]=0 )
+echo "SINGLET COMBINATION"
 for(( flip = 1; flip <= n; flip++ ))
 do
     echo -n "Flip-$flip is "
@@ -10,16 +12,51 @@ do
     if(( coin_flip_value == 0 ))
     then
         echo "Heads"
-        (( occurence_of_singlet[heads]++  ))
+        (( occurence_of_singlet[H]++  ))
     else
         echo "Tails"
-        (( occurence_of_singlet[tails]++  ))
+        (( occurence_of_singlet[T]++  ))
     fi
 done
+
 
 for combination in ${!occurence_of_singlet[@]}
 do
     count=${occurence_of_singlet[$combination]}
     percentage_of_combination=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
-    echo "percentage of $combination combination  is $percentage_of_combination"   
+    echo "percentage of $combination combination is $percentage_of_combination"   
+done
+
+echo 
+echo "DOUBLET COMBINATION"
+declare -A occurence_of_doublet=( [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
+for(( flip = 1; flip <= n; flip++ ))
+do
+    echo -n "Flip-$flip is "
+    (( coin_flip_value = RANDOM % 4 ))
+    case $coin_flip_value in
+        0)
+            echo "Heads Heads"
+            (( occurence_of_doublet[HH]++ )) 
+            ;;
+        1)
+            echo "Heads Tails"
+            (( occurence_of_doublet[HT]++ ))
+            ;;
+        2)
+            echo "Tails Heads"
+            (( occurence_of_doublet[TH]++ ))
+            ;;
+        3)
+            echo "Tails Tails"
+            (( occurence_of_doublet[TT]++ ))
+            ;;
+    esac
+done
+
+for combination in ${!occurence_of_doublet[@]}
+do
+    count=${occurence_of_doublet[$combination]}
+    percentage_of_combination=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
+    echo "percentage of $combination combination is $percentage_of_combination"   
 done
